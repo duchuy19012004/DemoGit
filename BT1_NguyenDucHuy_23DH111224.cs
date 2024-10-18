22ef6f6b3fff224f3abcc064d649a6ef9153614a
duchuy19012004 <zzcz4991@gmail.com>
2024-10-18 08:40:00 +0700

    Add files via upload

---
+++ BT1_NguyenDucHuy_23DH111224.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT1_23DH111224
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input string: ");
+            string stringNumber = Console.ReadLine();
+            int intNumber = int.Parse(stringNumber);
+            Console.WriteLine($"\"stringNumber\" value & type: {stringNumber}\n\"intNumber value\" & type: {intNumber.GetType()}");
+        }
+    }
+}

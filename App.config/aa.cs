73187c2a782274741a9c97e10b5ee99255ba821c
duchuy19012004 <zzcz4991@gmail.com>
2024-10-18 10:16:47 +0700

    Add files via upload

---
+++ App.config/aa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap mot so nguyen: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n > 0)
+        {
+            Console.WriteLine($"{n} la so duong");
+        }
+        else 
+        { 
+                Console.WriteLine($"{n} la so am");
+        }
+            if (n % 2 == 0)
+            {
+            Console.WriteLine($"{n} la so chan");
+            }
+        else
+        {
+            Console.WriteLine($"{n} la so le");
+        }
+    }
+}
+
+

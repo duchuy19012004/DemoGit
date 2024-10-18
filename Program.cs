e0cbdd445cbba336fab0af356a77aa7a019577fb
duchuy19012004 <zzcz4991@gmail.com>
2024-10-18 08:56:16 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,28 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bt2
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Nhap so nguyen thu nhat: ");
+            Console.Write("Nhap so nguyen a: ");
              int a = int.Parse(Console.ReadLine());
-            Console.Write("Nhap so nguyen thu hai: ");
+            Console.Write("Nhap so nguyen b: ");
             int b = int.Parse(Console.ReadLine());
-            Console.Write("Nhap so nguyen thu ba: ");
+            Console.Write("Nhap so nguyen c: ");
             int c = int.Parse(Console.ReadLine());
-           int max = a; //Gan a la so lon nhat
-            if (b > max) 
+            Console.WriteLine($"Before swapping,\na = {a} , b = {b} , c = {c}");
+            // Hoán đổi để a, b, c có giá trị tăng dần
+            if (a > b)
             {
-            max = b; 
+                int temp = a;
+                a = b;
+                b = temp;
             }
-            if (c > max)
+
+            if (a > c)
             {
-            max = c;
+                int temp = a;
+                a = c;
+                c = temp;
             }
-            Console.WriteLine($"So lon nhat giua 3 so nguyen la: {max}");
+
+            if (b > c)
+            {
+                int temp = b;
+                b = c;
+                c = temp;
+            }
+
+            // In giá trị sau khi hoán đổi
+            Console.WriteLine("\nAfter swapping:");
+            Console.WriteLine($"a = {a}, b = {b}, c = {c}");
         }
     }
 }

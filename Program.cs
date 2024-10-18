f42aac346fea15270b215911f79d39909dd5ec6f
nguyenduchuy19012004 <zzcz4991@gmail.com>
2024-10-18 08:27:49 +0700

    init baitap02

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen thu nhat: ");
+             int a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so nguyen thu hai: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so nguyen thu ba: ");
+            int c = int.Parse(Console.ReadLine());
+           int max = a; //Gan a la so lon nhat
+            if (b > max) 
+            {
+            max = b; 
+            }
+            if (c > max)
+            {
+            max = c;
+            }
+            Console.WriteLine($"So lon nhat giua 3 so nguyen la: {max}");
+            
+        }
+    }
+}

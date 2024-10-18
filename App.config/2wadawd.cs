4fa68ce5fb0ae297c0cb64c9d43bb0aa5d390e0d
duchuy19012004 <zzcz4991@gmail.com>
2024-10-18 15:36:24 +0700

    Add files via upload

---
+++ App.config/2wadawd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT1_23DH111224_NguyenDucHuy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen thu nhat: ");
+             int a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so nguyen thu hai: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a>b)
+            {
+                Console.WriteLine($"{a} la so lon nhat giua hai so");
+            }
+            else
+            {
+                Console.WriteLine($"{b} la so lon nhat giua hai so");
+            }
+        }
+    }
+}
